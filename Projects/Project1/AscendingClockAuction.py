ada6766ebb560f3e41fd558a2aa64f14b9c7006c
ander428 <ander428@mail.chapman.edu>
2020-11-03 10:31:11 -0800

    Add project 1

--- Projects/Project1/AscendingClockAuction.py
+++ Projects/Project1/AscendingClockAuction.py
@@ -1,19 +1,19 @@
+''' Improvements
+renamed monteCarloSimulation function to match naming convention
+added more comments to be more clear on code
+moved static variables to after function definitions
+'''
+
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-# inputs
-val_max = 100
-val_min = 0
-val_incr = 5
-trials = 100000
-
+# this runs a single clock auction given given 
+#   number of buyers, clock increment, and optionally reserve price
 def ascending_clock_auction(num_buyers, clock_incr, reserve_price=None):
     buyers = {}
     for i in range(num_buyers):
         buyers[f"Buyer {i+1}"] = random.choice(range(val_min, val_max, val_incr))
-
-    # print(buyers)
 
     clock_price = 0
     last_buyer_dropped = {}
@@ -22,31 +22,30 @@ def ascending_clock_auction(num_buyers, clock_incr, reserve_price=None):
     # end condition 1 - reserve price >= clock price
     while True:
         clock_price += clock_incr
-
-        # print("Clock Price:", clock_price)
         buyers, last_buyer_dropped = check_buyers(buyers, last_buyer_dropped, clock_price)
 
         # end condition 3 - clock price < reserve_price with a winner
+        # checks if reserve price is provided (defaults to None)
         if reserve_price and len(buyers) == 0 and clock_price < reserve_price:
             return 0
         
         # end condition 2 - one or less buyers
         elif len(buyers) == 0:
             winner_val = random.choice(list(last_buyer_dropped.values()))
-            # print(last_buyer_dropped)
             break
         elif len(buyers) == 1:
             winner_val = list(buyers.values())[0]
             break
 
     posted_price = clock_price - clock_incr
-
     return posted_price
 
+# helper function to check if a buyer will drop from an auction 
+# given the list of buyers and clock price
+# note: will also store the buyer in a new list if the buyer drops
 def check_buyers(buyers, last_buyer_dropped, clock_price):
     for buyer in list(buyers.keys()):
         if buyer in buyers.keys() and buyers[buyer] < clock_price:
-            # print(f"{buyer} has dropped out of the auction.")
             if len(last_buyer_dropped) == 0 or not buyers[buyer] in last_buyer_dropped.values():
                 last_buyer_dropped.clear()
             last_buyer_dropped[buyer] = buyers[buyer]
@@ -54,7 +53,7 @@ def check_buyers(buyers, last_buyer_dropped, clock_price):
             # print(last_buyer_dropped)
     return buyers, last_buyer_dropped
 
-
+# a print function that neatly prints the results of an auction simulation
 def print_results(num_buyers, clock_incr, avg_revenue, reserve_price=None):
     print("Number of buyers:", num_buyers)
     print("Clock Increment:", clock_incr)
@@ -63,7 +62,8 @@ def print_results(num_buyers, clock_incr, avg_revenue, reserve_price=None):
     print(f"Average Revenue: ${round(avg_revenue, 2)}")
     print()
 
-def monteCarloSimulation(num_buyers, clock_incr, reserve_price=None):
+# runs monte carlo simulation of ascending clock auctions
+def monte_carlo_sim(num_buyers, clock_incr, reserve_price=None):
     revenue = 0
     for _ in range(trials):
         revenue += ascending_clock_auction(num_buyers, clock_incr, reserve_price)
@@ -71,16 +71,22 @@ def monteCarloSimulation(num_buyers, clock_incr, reserve_price=None):
     print_results(num_buyers, clock_incr, revenue/trials, reserve_price)
     return revenue/trials
 
+# inputs
+val_max = 100
+val_min = 0
+val_incr = 5
+trials = 100000
+
 # A. What is the expected revenue if there are 5 buyers and the clock increment is $1?
 print("1a)")
-monteCarloSimulation(num_buyers=5, clock_incr=1)
+monte_carlo_sim(num_buyers=5, clock_incr=1)
 print("The expected revenue for 5 buyers and a $1 clock increment is around $64.23")
 print()
 
 # B. What is the expected revenue if there are 5 buyers and the clock increment is $10? 
 #  How did the increment affect the expected revenue and why does it have that effect?
 print("1b)")
-monteCarloSimulation(num_buyers=5, clock_incr=10)
+monte_carlo_sim(num_buyers=5, clock_incr=10)
 print("The expected revenue for 5 buyers and a $10 clock increment is around $61.70.")
 print("The average revenue is lower. This could be because when a buyer wins, the price paid is a posted price which is " +
         "one clock increment less than the clock price. Having a higher clock increment means that the posted price the buyer pays" + 
@@ -91,7 +97,7 @@ print()
 # C. What is the expected revenue if there are 20 buyers with a clock increment of $1? 
 #   Compare with item (a) and explain the difference.
 print("1c)")
-monteCarloSimulation(num_buyers=20, clock_incr=1)
+monte_carlo_sim(num_buyers=20, clock_incr=1)
 print("The expected revenue for 20 buyers and a $1 clock increment is around $87.95")
 print("The average revenue is way higher as the probability of a buyer valuing the item is much larger with more buyers. " +
         "This means that there will more often be competition at higher prices.")
@@ -101,7 +107,7 @@ print()
 #  Provide an illustration that supports your result.
 
 print("1d)")
-reserve_price_results = [(monteCarloSimulation(5, 5, reserve_price)) for reserve_price in range(0, 100, 5)]
+reserve_price_results = [(monte_carlo_sim(5, 5, reserve_price)) for reserve_price in range(0, 100, 5)]
 print(reserve_price_results)
 
 plt.bar(range(0, 100, 5), reserve_price_results)
--- Projects/Project1/VegasHotelProjections.py
+++ Projects/Project1/VegasHotelProjections.py
@@ -1,26 +1,40 @@
+
+''' Improvements
+renamed constructing to is_constructing to clarify it holds a boolean value
+shortened get_construction_profile to still be clear but more concise
+added comments for clarity
+fixed logical error in line 37, should be an elif not if
+'''
+
 import random
 
-# forcasting model
+# forcasts profit given a construction profile
+# T - number of years we are forcasting
 def get_profit(T, hotels):
     profit = 0
     for year in range(T):
         profit += (750 + 50*(year+1)) / (hotels[year]+1)
     return profit
 
-def get_construction_profile(T, m):
+# generate a profile of when hotels will be constructed
+def get_profile(T, m):
     profile = []
-    construction_time = m-1
-    constructing = False
+    construction_time = m-1 # the length it takes to build a hotel
+    is_constructing = False
     count = 0
 
+    # for every year, decided if hotel construction should start
     for t in range(T):
-        if not constructing and random.randint(0, 1) == 1:
-            constructing = True
+        # if not currently constructing, randomly choose to construct or not
+        if not is_constructing and random.randint(0, 1) == 1:
+            is_constructing = True
+        # finished constructing
         elif construction_time == 0:
             construction_time = m-1
-            constructing = False
+            is_constructing = False
             count += 1
-        if constructing:
+        # if constructing, then count current year towards construction
+        elif is_constructing:
             construction_time -= 1
         profile.append(count)
         
@@ -34,7 +48,7 @@ profits = []
 trials = 100000
 
 for _ in range(trials):
-    profile = get_construction_profile(T, m)
+    profile = get_profile(T, m)
     profits.append(get_profit(T, profile))
 
 print(f"With hotels taking {m} years to construct,")
